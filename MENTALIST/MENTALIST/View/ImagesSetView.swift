26b9da8c68d7e147e4150eb12bba6c702751c762
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-08-22 00:47:55 +0900

    📄 파일 생성

---
+++ MENTALIST/MENTALIST/View/ImagesSetView.swift
@@ -0,0 +1,19 @@
+//
+//  ImagesSetView.swift
+//  MENTALIST
+//
+//  Created by Jaehyeok Lim on 2023/08/22.
+//
+
+import UIKit
+
+class ImagesSetView: UIView {
+    
+    func cosmos_1() -> UIImageView {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(named: "cosmos_1")
+        
+        return imageView
+    }
+}
