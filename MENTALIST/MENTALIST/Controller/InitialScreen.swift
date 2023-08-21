2df38b961eeb517783c82e4ae489978356e649c1
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-08-22 00:46:58 +0900

    📄 ViewController 생성

---
+++ MENTALIST/MENTALIST/Controller/InitialScreen.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MENTALIST
+//
+//  Created by Jaehyeok Lim on 2023/08/21.
+//
+
+import UIKit
+
+class InitialScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
