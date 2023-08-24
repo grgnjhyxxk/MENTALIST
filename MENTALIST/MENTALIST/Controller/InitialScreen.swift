ca7f109625493b4de3f130f97b3baabe6df353ef
grgnjhyxxk <grgnjhyxxk@gmail.com>
2023-08-24 23:45:35 +0900

    ✏️ 레이아웃 수정

--- MENTALIST/MENTALIST/Controller/InitialScreen.swift
+++ MENTALIST/MENTALIST/Controller/InitialScreen.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class InitialScreen: UIViewController {
-
+    
+    var viewList: [UIView] = []
+    
+    let cosmos_1 = ImagesSetView().cosmos_1()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewLayout()
+    }
+    
+    private func addInViewList() {
+        viewList = [cosmos_1]
+        
+        for viewName in viewList {
+            view.addSubview(viewName)
+        }
+    }
+    
+    private func viewLayout() {
+        addInViewList()
+        
+        cosmos_1.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalTo(view)
+        }
     }
 }
 
